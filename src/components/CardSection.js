bc1510a5d8fe2fe58338294a45d9dfcf491dfe6c
beom <gb3187@naver.com>
2024-03-25 13:56:22 +0900

    refactor : css수정_1

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getFolder } from "../api";
-import searchIcon from "../assets/Search.svg";
 import CardList from "./CardList";
+import SearchBar from "./SearchBar";
 import "./CardSection.css";
 
 export default function CardSection() {
@@ -25,30 +25,20 @@ export default function CardSection() {
   return (
     <div className="cardSection">
       <div className="cardFrame">
-        <div className="searchBar">
-          <div className="searchBarElement">
-            <img id="searchIcon" src={searchIcon} alt="검색 아이콘" />
-            <input
-              type="text"
-              id="inputText"
-              placeholder="링크를 검색해보세요."
-            />
-          </div>
-        </div>
-        <ul className="card-list">
+        <SearchBar />
+        <div className="card-list">
           {cardList.map(({ id, createdAt, url, description, imageSource }) => {
             return (
-              <li key={id} className="card-item">
-                <CardList
-                  url={url}
-                  createdAt={createdAt}
-                  desc={description}
-                  imgUrl={imageSource}
-                />
-              </li>
+              <CardList
+                key={id}
+                url={url}
+                createdAt={createdAt}
+                desc={description}
+                imgUrl={imageSource}
+              />
             );
           })}
-        </ul>
+        </div>
       </div>
     </div>
   );
