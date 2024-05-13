35a8485c258ab341cb157db5d79d29188d6e5fa2
beom <gb3187@naver.com>
2024-05-13 17:52:49 +0900

    feat : [추가 구현사항] 실시간 링크 데이터 추가 기능

--- components/LinkAdd.tsx
+++ components/LinkAdd.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { addLink } from "@/pages/api/api";
 import styles from "@/styles/LinkAdd.module.css";
 import linkIcon from "@/public/images/link.svg";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 interface LinkData {
   url: string;
@@ -14,8 +14,15 @@ export default function LinkAdd() {
   const [url, setUrl] = useState(""); // input 값에 대한 상태
   const folderId = useQuery({ queryKey: ["folderId"] });
 
+  const queryClient = useQueryClient();
+
   const addLinkMutation = useMutation<void, Error, LinkData>({
     mutationFn: (linkData) => addLink(linkData.url, Number(linkData.folderId)),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["individualList", Number(folderId.data)],
+      });
+    },
   });
 
   // input 값이 변경될 때마다 상태 업데이트
@@ -29,7 +36,14 @@ export default function LinkAdd() {
     //링크를 추가하는 API 호출 등의 작업 수행
     if (folderId.data !== null) {
       const linkData: LinkData = { url, folderId: folderId.data };
-      addLinkMutation.mutate(linkData);
+      addLinkMutation.mutate(linkData, {
+        onSuccess: () => {
+          console.log("onSuccess in mutate");
+        },
+        onSettled: () => {
+          console.log("onSettled in mutate");
+        },
+      });
     }
   };
   return (
