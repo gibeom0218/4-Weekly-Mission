f0f39912b513c40951ea12bede4523c9d1c22bd4
beom <gb3187@naver.com>
2024-05-15 18:28:15 +0900

    feat : 폴더 추가 기능 구현

--- components/modal/AddFolder.tsx
+++ components/modal/AddFolder.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addFolder } from "@/pages/api/api";
 import styles from "@/styles/AddFolder.module.css";
 import closeIcon from "@/public/images/close.svg";
 
@@ -7,7 +9,39 @@ interface AddFolderProps {
   onClose: any;
 }
 
+interface FolderData {
+  name: string;
+}
+
 export default function AddFolder({ onClose }: AddFolderProps) {
+  const [name, setName] = useState(""); // input 값에 대한 상태
+  const queryClient = useQueryClient();
+
+  // input 값이 변경될 때마다 상태 업데이트
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const addFolderMutation = useMutation<void, Error, FolderData>({
+    mutationFn: (folderData) => addFolder(folderData.name),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["folderList"],
+      });
+    },
+  });
+
+  const clickChangeName = (name: string) => {
+    if (name) {
+      const folderData: FolderData = { name };
+      addFolderMutation.mutate(folderData, {
+        onSuccess: () => {
+          console.log("onSuccess in mutate");
+        },
+      });
+    }
+  };
+
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modalContainer}>
@@ -18,8 +52,14 @@ export default function AddFolder({ onClose }: AddFolderProps) {
           alt="closeIcon"
           onClick={onClose}
         />
-        <input placeholder="내용입력"></input>
-        <button>추가하기</button>
+        <input placeholder="내용입력" onChange={handleInputChange}></input>
+        <button
+          onClick={() => {
+            clickChangeName(name);
+          }}
+        >
+          추가하기
+        </button>
       </div>
     </div>
   );
--- pages/api/api.tsx
+++ pages/api/api.tsx
@@ -152,6 +152,31 @@ export async function changeName(name: string, folderId: number) {
   }
 }
 
+//폴더의 추가를 위한 api 함수
+export async function addFolder(name: string) {
+  const token = localStorage.getItem("accessToken");
+
+  try {
+    const response = await fetch(`${BASE_URL}/folders`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: name,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("폴더를 추가할 수 없습니다.");
+    }
+  } catch (error) {
+    throw new Error("폴더를 추가할 수 없습니다.");
+  }
+}
+
 //로그인 요청을 위한 api함수
 export async function postSignIn(id: string, password: string) {
   const response = await fetch(`${BASE_URL}/auth/sign-in`, {
