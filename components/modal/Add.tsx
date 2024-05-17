c9a81e12d2507156c39accb5a76a28e188da5966
beom <gb3187@naver.com>
2024-05-17 18:36:07 +0900

    feat : 폴더에 추가 모달창 오류 해결 (이미 폴더에 추가하는 기능이 있어 추가 기능은 구현 안함)

--- components/modal/Add.tsx
+++ components/modal/Add.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { getFolderList } from "@/pages/api/api";
+import { useQueryClient } from "@tanstack/react-query";
+//import { getFolderList } from "@/pages/api/api";
 import styles from "@/styles/Add.module.css";
 import checkIcon from "@/public/images/check.svg";
 import closeIcon from "@/public/images/close.svg";
@@ -13,14 +14,14 @@ interface AddProps {
 interface listType {
   name: string;
   id: number;
-  link: {
-    count: number;
-  };
+  link_count: number;
 }
 
 export default function Add({ linkUrl, onClose }: AddProps) {
-  const [list, setList] = useState([]);
+  const queryClient = useQueryClient();
   const [selectedItem, setSelectedItem] = useState(null); // 클릭한 항목의 인덱스를 저장하는 상태 변수
+
+  const list = queryClient.getQueryData<listType[]>(["folderList"]) || [];
 
   const clickListItem = (index: any) => {
     setSelectedItem(index); // 클릭한 항목의 인덱스를 상태 변수에 저장
@@ -30,19 +31,6 @@ export default function Add({ linkUrl, onClose }: AddProps) {
     console.log(list);
   };
 
-  useEffect(() => {
-    async function getList() {
-      try {
-        const { data } = await getFolderList();
-        setList(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    getList();
-  }, []);
-
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modalAddContainer}>
@@ -51,25 +39,26 @@ export default function Add({ linkUrl, onClose }: AddProps) {
           <p id={styles.subTitle}>{linkUrl}</p>
         </div>
         <div className={styles.listDivFrame}>
-          {list.map(({ name, id, link }: listType, index) => {
-            return (
-              <div
-                className={`${styles.listDiv} ${
-                  selectedItem === index ? styles.selected : ""
-                }`} // 클릭한 항목인 경우에는 'selected' 클래스를 추가
-                key={id}
-                onClick={() => clickListItem(index)} // 클릭 이벤트 핸들러 추가
-              >
-                <div className={styles.listContent}>
-                  <span id={styles.name}>{name}</span>
-                  <span id={styles.linkCount}>{`${link.count}개 링크`}</span>
+          {list &&
+            list.map(({ name, id, link_count }: listType, index: number) => {
+              return (
+                <div
+                  className={`${styles.listDiv} ${
+                    selectedItem === index ? styles.selected : ""
+                  }`} // 클릭한 항목인 경우에는 'selected' 클래스를 추가
+                  key={id}
+                  onClick={() => clickListItem(index)} // 클릭 이벤트 핸들러 추가
+                >
+                  <div className={styles.listContent}>
+                    <span id={styles.name}>{name}</span>
+                    <span id={styles.linkCount}>{`${link_count}개 링크`}</span>
+                  </div>
+                  {selectedItem === index && (
+                    <Image src={checkIcon} alt="checkIcon" />
+                  )}
                 </div>
-                {selectedItem === index && (
-                  <Image src={checkIcon} alt="checkIcon" />
-                )}
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
         <Image
           id={styles.closeIcon}
